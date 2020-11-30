7e6c7549b68733953947f783aead90fc3f84f4a0
Elisha Kramer <kramer1346@gmail.com>
2020-11-30 16:50:51 +0200

    added view of mah homepage on /manage route

--- next.config.js
+++ next.config.js
@@ -1,13 +1,40 @@
 require("dotenv").config();
-const withCSS = require('@zeit/next-css');
 const webpack = require('webpack');
-
 const apiKey = JSON.stringify(process.env.SHOPIFY_API_KEY);
 
-module.exports = withCSS({
-  webpack: (config) => {
+const withSass = require('@zeit/next-sass');
+const withCSS = require("@zeit/next-css");
+
+module.exports = withCSS(withSass({
+  publicRuntimeConfig: {
+  	API_KEY: apiKey,
+APP_NAME: 'My Favorite Products',
+API_DEVELOPMENT: 'http://localhost:8000/api',
+API_PRODUCTION: 'https://myamazonhistory.com/api',
+PRODUCTION: false,
+DOMAIN_DEVELOPMENT: 'http://localhost:3000',
+DOMAIN_PRODUCTION: 'https://myamazonhistory.com',
+FB_APP_ID: '305241670454834',
+DISQUS_SHORTNAME: 'ampitup-io',
+GOOGLE_CLIENT_ID:'299145764723-o5k1tr51r339gua3cja4o02r0l2g4lkb.apps.googleusercontent.com'
+},
+   webpack: function(config, options) {
     const env = { API_KEY: apiKey };
     config.plugins.push(new webpack.DefinePlugin(env));
+    
+    if (!options.dev) {
+        config.module.rules.push({
+            test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+            use: {
+                loader: 'url-loader',
+                options: {
+                    limit: 100000
+                }
+            }
+        });
+        return config;
+    }
+
     return config;
-  },
-});
+  }
+}));
